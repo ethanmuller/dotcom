15388425c3b9242f60450f1a11fdcac9a3670931
ethanmuller <ethan@heysparkbox.com>
2016-10-03 19:45:42 -0500

    docs (chore): Explain what's happening with gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
 
+// Right now, this is only copying
+// static files into the build directory.
+// Eventually I'll reverse engineer this to build
+// from sass, partials, etc.
 gulp.task('default', function() {
   return gulp.src('src/**/*')
     .pipe(gulp.dest('build/'));
